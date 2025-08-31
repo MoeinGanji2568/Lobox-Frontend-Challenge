31c6a7d3a6469e6ec89353e28c1ce3b7efc6484f
MoeinGanji2568 <moeinganji2004@gmail.com>
2025-08-31 11:48:52 +0330

    feat: integrate Redux store and toast notifications into the main application

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import "./styles/index.css";
+import { Provider } from "react-redux";
+import { store } from "./store/index.ts";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <Toaster />
+    <App />
+  </Provider>
+);
