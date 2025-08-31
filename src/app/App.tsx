5afce36186a591bf0315ce7e819d7ba8c9aa1232
MoeinGanji2568 <moeinganji2004@gmail.com>
2025-08-31 11:50:46 +0330

    feat: connect DropDown component to Redux state and option actions

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,16 @@
 import { DropDown } from "../components/ui/DropDown";
+import { useOptionActions } from "../hooks/useOptionAction";
+import { useOptionList } from "../hooks/useOptionList";
 
 function App() {
+  // for example get options from redux or api
+  const { options } = useOptionList();
+
+  // for example add new option from api
+  const { addNewOption } = useOptionActions();
   return (
     <section>
-      <DropDown />
+      <DropDown options={options} addNewOption={addNewOption} />
     </section>
   );
 }
